0ce307154442641d9db871581691a90ebe5ed891
Shelley Vohr <shelley.vohr@gmail.com>
2020-04-06 09:43:09 -0700

    chore: update hyper config

--- config_files/hyper.js
+++ config_files/hyper.js
@@ -1,27 +1,22 @@
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
-// See https://hyper.is#cfg for all currently supported options.
-
 module.exports = {
   config: {
     fontSize: 12,
-    fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"Hack Nerd Font", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
     cursorColor: '#888888',
     cursorShape: 'BEAM',
     cursorBlink: true,
+    webGLRenderer: false,
+
     foregroundColor: '#fff',
     backgroundColor: '#000436',
     borderColor: '#333',
+
     css: '',
-    termCSS: `
-      x-screen x-row {
-         font-variant-ligatures: contextual;
-       }
-    `,
-    commands: ['source ~/.dotfiles/.zshrc && cls'],
     showHamburgerMenu: '',
     showWindowControls: '',
     padding: '12px 14px',
+
     colors: {
       black: '#000000',
       red: '#888888',
@@ -40,12 +35,19 @@ module.exports = {
       lightCyan: '#8ae4f5',
       lightWhite: '#ffffff'
     },
-    shell: '',
+
+    shell: '/bin/zsh',
     shellArgs: ['--login'],
     env: {},
     bell: 'SOUND',
     copyOnSelect: false
   },
-  plugins: ["hyperlinks", "hyper-search", "hyperminimal", "hyper-startup"],
+  plugins: [
+    "hyperlinks",
+    "hyper-search",
+    "hyper-startup",
+    "hyperminimal",
+    "hyper-font-ligatures"
+  ],
   localPlugins: []
 };
